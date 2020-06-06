cc51a27e1be8004200a5e4d41969c3c81c9b0a03
SebaEm <seba_em@o2.pl>
2020-06-06 16:35:40 +0200

    Updates

---
+++ precache-manifest.6712aecc07a13ee6e54c76ce550c90b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff48b226b1c7c73b894d26f361aeb6e",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "d8f57a21bcba14ccf8ca",
+    "url": "/react-example/static/css/main.b3e8b766.chunk.css"
+  },
+  {
+    "revision": "7dd0288dac562ada2083",
+    "url": "/react-example/static/js/2.98aa46ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.98aa46ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8f57a21bcba14ccf8ca",
+    "url": "/react-example/static/js/main.c9070b86.chunk.js"
+  },
+  {
+    "revision": "91a7da182e845a375242",
+    "url": "/react-example/static/js/runtime-main.73f734a8.js"
+  }
+]);
